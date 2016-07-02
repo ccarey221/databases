b7e5571ff8089681ceb9ba7a3e8a72c4be2ae9f4
ccarey221 <careyc@union.edu>
2016-07-01 21:26:07 -0700

    Begin SQL connection

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,29 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'chill',
+  database: 'chat'
+});
 
+connection.connect(function(err) {
+  if (err) {
+    console.log('error connecting to db');
+  } else {
+    console.log('niiiice you are connected');
+  }
+});
+
+connection.query('SELECT * from messages', function(err, rows, fields) {
+  if (!err) {
+    console.log('The solution is: ', rows, fields);
+  } else {
+    console.log('Error');
+  }
+});
+
+connection.end(function(err) {
+  console.log('the connection ended peacefully');
+});
--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,27 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE rooms (
+  id int,
+  name varchar(25),
+  primary key(id)
+);
+
+CREATE TABLE users (
+  id int,
+  name varchar(25),
+  primary key(id)
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id int,
+  user int,
+  content varchar(250),
+  room int,
+  primary key(id),
+  foreign key(user) REFERENCES users(id),
+  foreign key(room) REFERENCES rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */
